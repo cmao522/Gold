76306454082c06fecfe6b2ed8439a1cb854b859a
Anna <agovor@uncc.edu>
2017-03-30 20:11:21 +0000

    added uncc logo to welcome page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,21 +23,21 @@ class PostsController < ApplicationController
         @post = Post.find(params[:id])
     end 
     
-    def update
-        @post = Post.find(params[:id])
-        if @post.update(post_params)
-            redirect_to @post
-        else 
-            render 'edit'
-        end
-    end 
+#    def update
+#        @post = Post.find(params[:id])
+#        if @post.update(post_params)
+#            redirect_to @post
+#         else 
+#            render 'edit'
+#         end
+#    end 
     
-    def destroy
-        @post = Post.find(params[:id])
-        @post.destroy
+#     #def destroy
+#         @post = Post.find(params[:id])
+#         @post.destroy
         
-        redirect_to posts_path
-    end
+#         redirect_to posts_path
+#     end
 end
 
 private
