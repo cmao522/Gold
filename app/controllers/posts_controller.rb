e3045e0e6c606e5112541bd2a822bdd3bb890ad8
Anna <agovor@uncc.edu>
2017-04-04 20:11:35 +0000

    Set up the create action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,43 +1,38 @@
 class PostsController < ApplicationController
     def index
-        # @posts = Post.all
+     #   @posts = Post.all
     end
     def show
-        # @post = Post.find(params[:id])
+     #   @post = Post.find(params[:id])
     end
     
     def new
-       # @post = Post.new
+     #  @post = Post.new
     end
     
     def create
-        # @post = Post.new(post_params)
-        # if @post.save
-        #   redirect_to @post
-        # else 
-       # render 'new'
-        # end
+      render plain: params[:post].inspect
     end
     
     def edit
-       # @post = Post.find(params[:id])
+     #   @post = Post.find(params[:id])
     end 
     
-#    def update
+   def update
 #        @post = Post.find(params[:id])
 #        if @post.update(post_params)
 #            redirect_to @post
-#         else 
+     #    else 
 #            render 'edit'
-#         end
-#    end 
+     #    end
+    end 
     
-#     #def destroy
+     def destroy
 #         @post = Post.find(params[:id])
 #         @post.destroy
         
 #         redirect_to posts_path
-#     end
+    end
 end
 
 private
