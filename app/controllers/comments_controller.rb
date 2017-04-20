fd34156b90bbac8214df3da72e785589efc4ce85
Anna <agovor@uncc.edu>
2017-04-20 20:27:12 +0000

    Add a comments form partial

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,14 +3,14 @@ def create
         @post = Post.find(params[:post_id])
         @comment = @post.comments.create(comment_params)
         redirect_to post_path(@post)
-    end
+end
 
-#def destroy 
-#    @post = Post.find(params[:post_id])
-#    @comment = @post.comments.find(params[:id])
-#    @comment.destroy
-#    redirect_to post_path(@post)
-#end
+def destroy 
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+    redirect_to post_path(@post)
+end
     
 private
         def comment_params
@@ -18,3 +18,4 @@ private
         end
         
 end
+
