ec454d492f2237ec067b813505222e97b8ef0073
srasomba <srasomba@uncc.edu>
2017-05-02 21:03:10 +0000

    Update homepage and update CSS

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do   
-    provider :google_oauth2, 'my Google client id', 'my Google client secret', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}} 
+    provider :google_oauth2, '1095313095929-i7bajihdoi9ge7pcn7ls4mm4lmm0rtej.apps.googleusercontent.com', 'uWldwF7Oh-7uXbKtUbWQyVdD', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}} 
 end
 
